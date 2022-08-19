37d907162684e63a25bcda7a7f200ae656a43224
CoBeTcKuu <9437855@gmail.com>
2022-08-19 22:12:18 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
+        //Task 1
+        String firstName = "Ivanov";
+        String middleName = " Ivan ";
+        String lastName = "Ivanovich";
+        String fullName = firstName + middleName + lastName;
+        System.out.println(fullName);
     }
 }
